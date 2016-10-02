b95805151b70cca4b84de2f687a6486f762d9141
Eugene Gladyshev <eugen.gladyshev@gmail.com>
2016-10-02 23:39:53 +0300

    Test git

--- main.rb
+++ main.rb
@@ -3,14 +3,13 @@ class Fish
 end
 
 class Pike < Fish
-	attr_accessor :weight
 
-	def initializer
-		@weight = 3
-	end
+  attr_accessor :weight
 
+  def initializer
+    @weight = 3
+  end
 end
-
 
 class Carp < Fish
 
